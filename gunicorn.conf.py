31c6202eeb550a851fb20bf2e70d151bd8aa3bd2
ifayush <103126856+ifayush@users.noreply.github.com>
2025-06-18 22:26:57 +0530

    new files

---
+++ gunicorn.conf.py
@@ -0,0 +1,8 @@
+# Gunicorn configuration file
+bind = "0.0.0.0:10000"
+workers = 2
+worker_class = "uvicorn.workers.UvicornWorker"
+timeout = 120
+keepalive = 2
+max_requests = 1000
+max_requests_jitter = 100 
